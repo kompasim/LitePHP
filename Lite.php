80d2237e053f4f37cae2ff10f08554f5e8f67bad
kompasim <kompasim@163.com>
2022-06-25 00:56:39 +0800

    add func callback

--- Lite.php
+++ Lite.php
@@ -17,7 +17,8 @@ class Lite
 
     function __construct()
     {
-        //
+        $this->routes = [];
+        $this->func = NULL;
     }
 
     function __destruct()
@@ -25,22 +26,38 @@ class Lite
         //
     }
 
-    function run()
+    function run($argument)
     {
+        if(is_array($argument)) $this->routes = $argument;
+        if(is_callable($argument)) $this->func = $argument;
         // filter
         $path = trim($_SERVER['PATH_INFO'], "/");
         assertOrExit(is_string($path), "path error!");
         // parse
         $describe = trim(trim($this->parse($path), "/"), "?");
         assertOrExit(isValidString($describe), "route error!");
-        // excecute
-        $this->execute($describe);
+        // check parameters
+        $args = explode('/', $describe);
+        assertOrExit(count($args) >= 2, 'system error!');
+        $class = $args[0];
+        $method = $args[1];
+        define("CURRENT_APPLICATION", $class);
+        define("CURRENT_FUNCTION", $method);
+        $params = array_slice($args, 2, count($args) - 2);
+        // run
+        if ($this->func != NULL) {
+            // execute
+            $this->execute($class, $method, $params);
+        } else{
+            // dispatch
+            $this->dispatch($class, $method, $params);
+        }
     }
 
     function parse($path)
     {
         // router
-        foreach (ROUTES_MAP as $route => $describe)
+        foreach ($this->routes as $route => $describe)
         {
             assertOrExit(preg_match("/^.+$/i", $route), "invaid route key!");
             assertOrExit(preg_match("/^(\w+)(\/\w+)$/i", $describe), "invaid route value!");
@@ -55,18 +72,18 @@ class Lite
         return NULL;
     }
 
-    function execute($describe)
+    function execute($class, $method, $params)
     {
-        // check parameters
-        $args = explode('/', $describe);
-        assertOrExit(count($args) >= 2, 'system error!');
-        $class = $args[0];
-        $method = $args[1];
-        define("CURRENT_APPLICATION", $class);
-        define("CURRENT_FUNCTION", $method);
-        $params = array_slice($args, 2, count($args) - 2);
-        $file = PATH_APP . $class . '.php';
+        // instantiate application
+        $app = new LApplication($this);
+        call_user_func_array($this->func, array($app, $class, $method, $params));
+        return $app;
+    }
+
+    function dispatch($class, $method, $params)
+    {
         // require file
+        $file = PATH_APP . $class . '.php';
         assertOrExit(file_exists($file), 'file not found:' . $file);
         require_once $file;
         // instantiate class
@@ -75,6 +92,7 @@ class Lite
         // call method
         assertOrExit(method_exists($object, $method), 'method not found:' . $method);
         call_user_func_array(array($object, $method), $params);
+        return $object;
     }
 
 }
--- template/index.php
+++ template/index.php
@@ -2,13 +2,38 @@
 
 // routes
 
-define('ROUTES_MAP', [
+$route = [
     '\s*' => 'App/default', // default
     'app/hello' => 'App/hello', // example: index.php/app/hello/Emily
-    'app/hi' => 'App/hi', // example: index.php/app/hi?name=Alice
+    'app/hiii' => 'App/hi', // example: index.php/app/hi?name=Alice
     '.*' => 'App/empty', // 404
-]);
+];
 
 include_once("../Lite.php");
 $lite = new Lite();
-$lite->run();
+$lite->run($route);
+
+// 
+/**
+ * run with defined route rule, choose the class and method name according to the rule and call the mathing method of the class
+ * example: http://test.xyz/test/index.php/class/method/argument1/argument2
+ * @param route
+ * @return application
+ */
+// $lite->run($route);
+
+/**
+ * run with default directory structure, find class in this directory and call the matching method
+ * example: http://test.xyz/test/index.php/class/method/argument1/argument2
+ * @param NULL
+ * @return application
+ */
+// $lite->run(NULL);
+
+/**
+ * run with a powerfull handler function, parse url and create LApplication then call the callback
+ * example: http://test.xyz/test/index.php/class/method/argument1/argument2
+ * @param function (app, class, method, arguments)
+ * @return application
+ */
+// $lite->run(function($app, $class, $method, $arguments) {});
