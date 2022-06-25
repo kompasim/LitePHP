76686cc4b4321433d70017c59a9c42ba2484971f
kompasim <kompasim@163.com>
2022-06-25 14:05:58 +0800

    update response

--- lite/LResponse.php
+++ lite/LResponse.php
@@ -15,18 +15,23 @@ class LResponse
         //
     }
 
-    function writeRedirect($path)
+    function writeRedirect($url, $code = 303)
     {
-        assert_valid_string($path);
-        $url = $_SERVER['SCRIPT_NAME'] . "/" . $path;
-        echo "<script>window.location.href='" . $url . "';</script>";
-        exit;
+        assert_valid_string($url);
+        if(!preg_match("/^https?:\/\/\w+\.\w+.*$/i", $url)) $url = $_SERVER['SCRIPT_NAME'] . "/" . $url;
+        header('Location:' . $url, true, $code);
     }
 
     function writeHead($content)
     {
         assert_valid_string($content);
         header($content);
+    }
+
+    function writeCode($code = 200)
+    {
+        assert_valid_number($code);
+        http_response_code($code);
     }
 
     function writeText($text)
@@ -44,17 +49,39 @@ class LResponse
         }
         require $path;
     }
-
-    function writeFile($path)
-    {
-        assert_valid_file($path);
-        echo file_get_contents($path);
-    }
-
+    
     function writeJson($array)
     {
         assert_valid_array($array);
+        header('Content-type: application/json');
         echo json_encode($array);
+    }
+    
+    function writeFile($path)
+    {
+        $path = PATH_APP . $path;
+        assert_valid_file($path);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $ftype = finfo_file($finfo, $path);
+        finfo_close($finfo);
+        $fp = fopen($path, 'rb');
+        header("Content-Type: " . $ftype);
+        header("Content-Length: " . filesize($path));
+        fpassthru($fp);
+    }
+
+    function writeDownload($path, $name = NULL)
+    {
+        $path = PATH_APP . $path;
+        assert_valid_file($path);
+        if (!is_valid_string($name)) $name = basename($path);
+        header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+        header("Cache-Control: public"); // needed for internet explorer
+        header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: Binary");
+        header("Content-Length:".filesize($path));
+        header("Content-Disposition: attachment; filename=" . $name);
+        readfile($path);
     }
 
 }
